be46ab163ce72c2b1bf98ae5738fdc7bde39b080
Marco Antonio Chi Castillo <chimarcoantonio@gmail.com>
2020-08-07 19:10:05 -0500

    Agregando Docs para GitHub

---
+++ docs/precache-manifest.2a8c0d35b9984a9471c5eb36ff866816.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5379eeaae2cffdb3b9d3d4f3106159c0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4288db11647e4c0b78ad",
+    "url": "/static/css/main.84349461.chunk.css"
+  },
+  {
+    "revision": "9d0467bdd98391920114",
+    "url": "/static/js/2.b04e268c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.b04e268c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4288db11647e4c0b78ad",
+    "url": "/static/js/main.1290b9fc.chunk.js"
+  },
+  {
+    "revision": "daee87f482498cdf724c",
+    "url": "/static/js/runtime-main.29242b1d.js"
+  }
+]);
